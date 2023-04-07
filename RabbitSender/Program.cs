9a9fdc0f6ede11efd4cf77d8e38b15d87644b302
swati28-sj <swatijha267@gmail.com>
2023-04-08 01:00:34 +0530

    Init

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello Tushar");
+channel.BasicPublish(exchangeName,routingKey,null,messageBodyBytes);
+
+channel.Close();
+cnn.Close();
