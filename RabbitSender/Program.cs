87b8755982bef8452be577f1b5f913a94c2c1c14
swati28-sj <swatijha267@gmail.com>
2023-04-08 01:52:25 +0530

    second Reciver Added

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -17,8 +17,12 @@ channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
 channel.QueueDeclare(queueName, false, false, false, null);
 channel.QueueBind(queueName, exchangeName, routingKey, null);
 
-byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello Tushar");
-channel.BasicPublish(exchangeName,routingKey,null,messageBodyBytes);
+for(int i=0;i<60;i++)
+{ 
 
-channel.Close();
+    Console.WriteLine($"Sending Message {i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message #{i}");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    Thread.Sleep(1000); 
+}
 cnn.Close();
